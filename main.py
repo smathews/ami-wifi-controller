dfe41c61d4bd69658d86d6a0e1062025928cc197
Stephen Mathews <mathews.stephen@gmail.com>
2022-12-04 09:49:42 -0700

    dynamic strips

--- main.py
+++ main.py
@@ -3,6 +3,39 @@ import network
 import logger
 import os
 
+song_collection = [
+    {
+        "artist": "Art of Noise",
+        "color": "green",
+        "side_a": "That's What You Think",
+        "side_b": "To The Edit",
+    },
+    {
+        "artist": "Art of Noise",
+        "color": "pink",
+        "side_a": "Paranoid",
+        "side_b": "Peter Gunn",
+    },
+    {
+        "artist": "LCD Soundsystem",
+        "color": "blue",
+        "side_a": "Us V Them",
+        "side_b": "I Can Change",
+    },
+    {
+        "artist": "Missing Persons",
+        "color": "green",
+        "side_a": "Nobody Walks in LA",
+        "side_b": "US Drag",
+    },
+    {
+        "artist": "The Police",
+        "color": "pink",
+        "side_a": "Synchronicity",
+        "side_b": "Synchronicity II",
+    },
+]
+
 ap = network.WLAN(network.AP_IF)
 ap.config(essid="foobar", password="foobarfoobar")
 ap.active(True)
@@ -29,7 +62,7 @@ app.pkg = None
 @app.route("/")
 def index(req, resp):
     yield from picoweb.start_response(resp)
-    yield from app.render_template(resp, "titlestrips.tpl.html", ({},))
+    yield from app.render_template(resp, "titlestrips.tpl.html", (song_collection,))
 
 # Send our logger in, ulogger is the default (its dependencies are too large for rpico)
 app.run(log=logger, debug=True, host="0.0.0.0", port=80)
